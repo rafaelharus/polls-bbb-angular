4f7a6af6ffa278169adaee3f13540c907df95343
Rafael Harus <rafael.cruz@zooxsmart.com>
2022-06-23 12:43:04 -0300

    fix(environment): change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://pollbs-bbb-api.local:8080',
+  apiUrl: 'http://polls-bbb-api.local:8080',
   pollId: '95d99e78-38ce-41a1-b3a5-c507d1040ca2',
   nhcaptchaSiteKey: '3d697f74-633f-4abe-861a-47f4ac8b5dcd',
   apiKey: 'Basic OTVlNjUwMzEwYWE0MDFiNTg1MmQ2M2U1OWFhM2EwY2M6NjA4NzQzMjM3ZTRmYWE4MTYzYzdhMzMxMjE4MDI5OTY='
